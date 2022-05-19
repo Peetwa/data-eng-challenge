9f871b91120b5cba5e1d0990c079e2f743f82802
Peter Carras <Peter.Carras@gfs.com>
2022-05-18 23:36:09 -0400

    Adding broken Part 2 changes

--- dbt/models/base/nhl/nhl_players.sql
+++ dbt/models/base/nhl/nhl_players.sql
@@ -1,3 +1,15 @@
 select
-  * -- TODO replace this with correct columns
+  nhl_player_id as id,
+  full_name,
+  game_team_name as team_name,
+  stats_assists as assists,
+  stats_goals as goals,
+  stats_goals + stats_assists as points
 from {{ ref('player_game_stats') }} --or whatever other table reference
+where
+nhl_player_id is not null and
+full_name is not null and
+game_team_name is not null and
+stats_assists is not null and
+stats_goals is not null
+
--- dbt/models/mart/nhl/points_leaders.sql
+++ dbt/models/mart/nhl/points_leaders.sql
@@ -1,3 +1,7 @@
 select 
-* -- TODO replace with correct projection columns
+team_name,
+full_name,
+max(points) as points
 from {{ ref('nhl_players') }}  -- or other tables
+group by full_name, team_name
+order by points
